238f8da17281084faa219135e41d4f05c37f7d8b
Abrahan Mendoza <mendozaabrahan@gmail.com>
2020-01-10 19:16:19 +0100

    Reformated code and more

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def new
     @article = Article.new
   end
@@ -41,11 +42,12 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
