14e5cb3ba84ad59a090b8bea87696013ebc734d3
Abrahan Mendoza <mendozaabrahan@gmail.com>
2020-01-11 18:14:16 +0100

    Add edit update functionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+
   def new
     @user = User.new
   end
@@ -13,7 +16,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'Your account was updated successfully'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
